fe6542b12af5c0e8646b571e623d2619cb180246
LEGOLLAS <gurwl1885@gmail.com>
2018-06-01 10:43:12 +0900

    새롭게 시작

--- dev/js/server_side_javascript/hello.js
+++
@@ -1,2 +0,0 @@
-console.log('hello world');//디버깅 할 시에 콘솔창에 데이터를 출력해보고 싶을때에 사용.
-console.log(1+10);
--- dev/js/server_side_javascript/uglified.js
+++
@@ -1,2 +0,0 @@
-//uglifyjs pretty.js -o uglified.js -m 명령어를 사용하여 uglify화 한 코드들을 파일형태로 저장한다. 
-function hello(l){console.log("Hi,"+l)}hello("hjh");
