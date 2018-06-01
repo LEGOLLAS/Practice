4eefcedb8e39d4d98b290e2c67ae967e7f29f11d
LEGOLLAS <gurwl1885@gmail.com>
2018-06-01 12:48:18 +0900

    practice

--- practice_PHP/test.php
+++ practice_PHP/test.php
@@ -1,3 +1,5 @@
 <?php
-  print "hello";
+  print "<META http-equiv = 'refresh' content='5;
+  URL= https://www.naver.com/'>";
+  print "5초 후에 이동합니다.";
 ?>
---
+++ practice_PHP/veriable.php
@@ -0,0 +1,20 @@
+<?
+#변수는 $를 사용한다.
+?>
+<?
+  $a ="hello";
+  print $a;
+  print "<br>";
+?>
+<?
+  print M_PI;
+  print "<br>";
+?>
+<?
+  print PHP_VERSION;
+  print "<br>";
+?>
+<?
+  print PHP_OS;
+  print "<br>"
+?>
