bb79e9bc04f0a953db1e5f1767d9e0a98c378b85
LEGOLLAS <gurwl1885@gmail.com>
2018-07-20 17:21:24 +0900

    dafxvzv

--- practice_Javascript/index.js
+++ practice_Javascript/index.js
@@ -1,3 +1,9 @@
-var pi = 3.1415;
-var radius = 10;
-alert(2*pi*radius);
+window.onload = function(){
+  var list = '';
+  list += '<ul>';
+  list += '     <li>Hello</li>';
+  list += '     <li>Javascript</li>';
+  list += '</ul>';
+
+  document.body.innerHTML = list;
+}
